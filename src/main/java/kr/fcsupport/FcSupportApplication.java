10eefb0143f78c2eb1e35f5dc32dbb1f6887676e
kinngh00 <kinngh0.0@gmail.com>
2026-01-30 22:44:59 +0900

    feat: initial project setup for FC-Support with PR template

---
+++ src/main/java/kr/fcsupport/FcSupportApplication.java
@@ -0,0 +1,13 @@
+package kr.fcsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/fcsupport/FcSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.fcsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
